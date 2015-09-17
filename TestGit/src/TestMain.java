fbfe8cffe34441a03dd5b8e8e2372742b6dffb8d
ManuFav <emmanuel.guittard@gmail.com>
2015-09-17 16:44:21 +0200

    2eme commit

--- TestGit/src/TestMain.java
+++ TestGit/src/TestMain.java
@@ -3,7 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("coucou");
+		System.out.println("coucou toi");
 	}
 
 }
