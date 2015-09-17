5744bb437b9427f446976a31da91b01470ad64e1
mica91 <miica.adele@gmail.com>
2015-09-17 15:28:58 +0200

    Mon 2eme commit

--- OutilCollaboratif_GIT/src/Main.java
+++ OutilCollaboratif_GIT/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("OUTIL COLLABORATIIIIIIIIIF");
+		System.out.println("lol OUTIL COLLABORATIIIIIIIIIF");
 	}
 
 }
