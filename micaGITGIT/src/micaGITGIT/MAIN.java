28089ac67ce8a5171e1081a687160a0a05dcb0ae
mica91 <miica.adele@gmail.com>
2015-09-17 17:06:58 +0200

    commit projet

---
+++ micaGITGIT/src/micaGITGIT/MAIN.java
@@ -0,0 +1,12 @@
+package micaGITGIT;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bla bla bla");
+	}
+
+}
